401ff2f9ec26b417bcba09abae79313283271bd5
dlausseur <dlausseur@gmail.com>
2019-06-19 15:37:50 +0200

    ajout du premier script js boite à outils

---
+++ src/Migrations/Version20190619093257.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190619093257 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE event (id INT AUTO_INCREMENT NOT NULL, streamer_id INT NOT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(500) DEFAULT NULL, start DATETIME NOT NULL, end DATETIME NOT NULL, INDEX IDX_3BAE0AA725F432AD (streamer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, name VARCHAR(100) NOT NULL, password VARCHAR(60) NOT NULL, email VARCHAR(320) NOT NULL, active SMALLINT NOT NULL, type SMALLINT NOT NULL, token VARCHAR(32) DEFAULT NULL, INDEX IDX_8D93D649A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE event ADD CONSTRAINT FK_3BAE0AA725F432AD FOREIGN KEY (streamer_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE event DROP FOREIGN KEY FK_3BAE0AA725F432AD');
+        $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D649A76ED395');
+        $this->addSql('DROP TABLE event');
+        $this->addSql('DROP TABLE user');
+    }
+}
